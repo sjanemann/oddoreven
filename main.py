ee7c89ea8c0d2c8151569a424075a3c336c8c70f
sjanemann <sjmann0516@icloud.com>
2022-05-30 13:58:43 -0400

    is the number even or odd

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
 # Odd or Even
 # Samantha Mann
+number = int(input("please select a number"))
+
+if number % 2 == 0:
+    print("number is even")
+if number % 2 == 1:
+    print("number is odd like u")
